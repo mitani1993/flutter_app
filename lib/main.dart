e4193259940ee967c59a12bb1db7e3f4f3580d9f
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2022-08-10 15:34:39 +0900

    FloatingActionButton

--- lib/main.dart
+++ lib/main.dart
@@ -16,48 +16,12 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-          body: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(top: 32),
-            child: Text('IconButton'),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              IconButton(onPressed: () {}, icon: Icon(Icons.thumb_up)),
-              IconButton(
-                  color: Colors.pink,
-                  onPressed: () {},
-                  icon: Icon(Icons.favorite)),
-              IconButton(
-                  iconSize: 64, onPressed: () {}, icon: Icon(Icons.flight)),
-            ],
-          ),
-          Container(
-            padding: EdgeInsets.only(top: 32),
-            child: Text('アイコンテキスト'),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              TextButton.icon(
-                onPressed: () {},
-                icon: Icon(Icons.thumb_up),
-                label: Text('Good'),
-              ),
-              OutlinedButton.icon(
-                  onPressed: () {},
-                  icon: Icon(Icons.favorite, color: Colors.pink),
-                  label: Text('Like')),
-              ElevatedButton.icon(
-                  onPressed: () {},
-                  icon: Icon(Icons.flight),
-                  label: Text('Flight')),
-            ],
-          ),
-        ],
-      )),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.green,
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+      ),
     );
   }
 }
